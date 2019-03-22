92784bacaf35d720e331402dacf35aa20b77aab7
aclouditation <25907312+AClouDitation@users.noreply.github.com>
2019-03-21 20:44:27 -0400

    bugfix and added perceptron

--- hw04/py/exercise2.py
+++ hw04/py/exercise2.py
@@ -31,10 +31,6 @@ def import_data():
     samples = np.array(np.loadtxt('../data/hw04_sample_vectors.csv', delimiter=','))
     labels = np.array(np.loadtxt('../data/hw04_labels.csv', delimiter=','))
 
-    # fig, ax = newfig()
-    # ax.plot(samples[:,0], samples[:,1], '.')
-    # plt.show()
-
     return samples, labels
 
 
@@ -66,7 +62,44 @@ def logistic(X, labels, learning_rate=0.1, max_num_iterations=200):
     final_adjust('../pix/logistic.pdf')
 
 
+def perceptron(X, labels, learning_rate=0.1, max_num_iterations=200, batchMode = False):
+
+    theta = np.array([0, 0.0001, 0], dtype=np.float64)
+
+    fig, ax = newfig()
+    lenl1 = int(sum(labels))
+    lenl0 = len(labels) - lenl1
+
+    ax.plot(X[:lenl0,0], X[:lenl0,1], '.')
+    ax.plot(X[lenl1:,0], X[lenl1:,1], '.')
+    ax.axis([-1.3,1.3,-0.7,0.5])
+
+    for m in range(max_num_iterations):
+        shuffled_index = np.random.permutation(labels.size)
+        X = X[shuffled_index, :]
+        labels = labels[shuffled_index]
+
+        misclassified = False
+        for i, label in enumerate(labels):
+            if np.dot(theta, np.concatenate([X[i], [1]]).T) * (1.0 if label > 0 else -1.0) < 0:
+                theta += learning_rate * (1.0 if label>0 else -1.0) * np.concatenate([X[i], [1]])
+                misclassified = True
+                if not batchMode: break
+
+        if m % (max_num_iterations / 5) == 0: 
+            drawline(ax, theta)
+
+        if not misclassified:
+            break
+
+    drawline(ax, theta)
+    fn = '../pix/perceptron_' + ('batch' if batchMode else 'online') + '.pdf'
+    final_adjust(fn)
+
+
 if __name__ == "__main__":
 
     X, labels = import_data()
-    logistic(X, labels)
+    #logistic(X, labels)
+    perceptron(X, labels)
+    perceptron(X, labels, batchMode=True)
